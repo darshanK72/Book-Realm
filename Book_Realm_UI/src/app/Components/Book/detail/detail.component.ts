225077b7b410d67fafeefa24612275bad05dc29f
Darshan Khairnar <darshankhairnar72@gmail.com>
2024-03-06 22:18:24 +0530

    added related books section at book detail page

--- Book_Realm_UI/src/app/Components/Book/detail/detail.component.ts
+++ Book_Realm_UI/src/app/Components/Book/detail/detail.component.ts
@@ -2,13 +2,17 @@ import { HttpClient } from '@angular/common/http';
 import {
   Component,
   ElementRef,
+  Input,
   OnInit,
   Renderer2,
   ViewChild,
 } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { select, Store } from '@ngrx/store';
 import { Book } from 'src/app/Models/book';
 import { ScrollService } from 'src/app/Services/scroll/scroll.service';
+import { AppState } from 'src/app/Store/app.state';
+import { selectBooks } from 'src/app/Store/book/book.selectors';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -17,12 +21,17 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./detail.component.css'],
 })
 export class DetailComponent implements OnInit {
+
   constructor(
     private renderer: Renderer2,
     private http: HttpClient,
     private route: ActivatedRoute,
-    private scrollService: ScrollService
+    private scrollService: ScrollService,
+    private store: Store<AppState>,
+    private router:Router
   ) {}
+
+  books$ = this.store.pipe(select(selectBooks));
 
   @ViewChild('desc')
   desc!: ElementRef;
@@ -36,16 +45,25 @@ export class DetailComponent implements OnInit {
   show: boolean = false;
 
   book!: Book;
+  bookId!:number;
 
   baseUrl: string = environment.baseUrl;
 
-  ngOnInit(): void {
-    let id = parseInt(this.route.snapshot.paramMap.get('bookId') || '');
+  relatedBooks!:Book[];
 
-    this.http.get<Book>(`${this.baseUrl}/book/${id}`).subscribe((data) => {
-      this.book = data;
-      console.log(this.book);
-    });
+  ngOnInit(): void {
+    
+    this.route.paramMap.subscribe((params) => {
+      this.bookId = parseInt(params.get('bookId') || '');
+
+      this.http.get<Book>(`${this.baseUrl}/book/${this.bookId}`).subscribe((data) => {
+        this.book = data;
+        console.log(this.book);
+       });
+  
+       this.books$.subscribe((data) => this.relatedBooks = data.filter((book) => book.subgenreId == 16));
+    })
+  
   }
 
   readMore() {
@@ -63,5 +81,10 @@ export class DetailComponent implements OnInit {
   changeImage(link: string, mainImage: HTMLDivElement) {
     let im = mainImage.firstElementChild;
     im?.setAttribute('src', link);
+  }
+
+  getBookDetail(i:number){
+    console.log(i);
+    this.router.navigate(["/detail",i])
   }
 }

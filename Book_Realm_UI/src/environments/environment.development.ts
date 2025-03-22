ae51b2d41dd2d872846c08a33a915dad085504b7
darshan72 <darshankhairnar72@gmail.com>
2025-03-22 17:36:37 +0530

    deployed latest version of api with angular

--- Book_Realm_UI/src/environments/environment.development.ts
+++ Book_Realm_UI/src/environments/environment.development.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production:false,
     // baseUrl:"http://localhost:3000"
-    authUrl:"https://localhost:7175/api",
+    authUrl:"https://book-realm.onrender.com/api",
     // baseUrl:"https://book-realm-json-server.onrender.com",
-    baseUrl:"https://localhost:7175/api",
+    baseUrl:"https://book-realm.onrender.com/api",
     googleClientId:"594617159900-6pi5md0ud2c620iif0rm2h17gbflsb8v.apps.googleusercontent.com",
     googleClientName:"Book-Realm-Google-Auth",
 };
--- Book_Realm_UI/src/environments/environment.ts
+++ Book_Realm_UI/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production:true,
-    baseUrl:"https://localhost:7175/api",
-    authUrl:"https://localhost:7175/api",
+    baseUrl:"https://book-realm.onrender.com/api",
+    authUrl:"https://book-realm.onrender.com/api",
     googleClientId:"594617159900-6pi5md0ud2c620iif0rm2h17gbflsb8v.apps.googleusercontent.com",
     googleClientName:"Book-Realm-Google-Auth",
 };

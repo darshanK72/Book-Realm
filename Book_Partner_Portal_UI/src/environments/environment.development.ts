a6fd5435ea0791e059585e1e8ce5dd2aba2d08e3
Darshan Khairnar <darshankhairnar72@gmail.com>
2026-01-02 13:16:29 +0530

    feat: switch API base and auth URLs from Azure to localhost in environment configurations.

--- Book_Partner_Portal_UI/src/environments/environment.development.ts
+++ Book_Partner_Portal_UI/src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    baseUrl: "https://bookrealmapi-fxftcph4b5fwfhgw.canadacentral-01.azurewebsites.net/api",
-    authUrl: "https://bookrealmapi-fxftcph4b5fwfhgw.canadacentral-01.azurewebsites.net/api",
+    baseUrl: "http://localhost:7175/api",
+    authUrl: "http://localhost:7175/api",
     googleClientId: "594617159900-6pi5md0ud2c620iif0rm2h17gbflsb8v.apps.googleusercontent.com",
     googleClientName: "Book-Realm-Google-Auth",
 };
--- Book_Realm_UI/src/environments/environment.development.ts
+++ Book_Realm_UI/src/environments/environment.development.ts
@@ -1,7 +1,9 @@
 export const environment = {
     production: false,
-    baseUrl: "https://bookrealmapi-fxftcph4b5fwfhgw.canadacentral-01.azurewebsites.net/api",
-    authUrl: "https://bookrealmapi-fxftcph4b5fwfhgw.canadacentral-01.azurewebsites.net/api",
+    baseUrl: "http://localhost:5239/api",
+    authUrl: "http://localhost:5239/api",
+    // baseUrl: "https://bookrealmapi-fxftcph4b5fwfhgw.canadacentral-01.azurewebsites.net/api",
+    // authUrl: "https://bookrealmapi-fxftcph4b5fwfhgw.canadacentral-01.azurewebsites.net/api",
     googleClientId: "594617159900-6pi5md0ud2c620iif0rm2h17gbflsb8v.apps.googleusercontent.com",
     googleClientName: "Book-Realm-Google-Auth",
 };
